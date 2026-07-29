c56abc4b6a56effe30a232b555a280004c7c7e32
anglobaltech <mqtltech@gmail.com>
2026-07-29 17:43:55 +0530

    stock updated

--- controllers/stockController.js
+++ controllers/stockController.js
@@ -56,7 +56,7 @@ exports.createGateEntry = asyncHandler(async (req, res) => {
   } = req.body;
 
   const gateEntryId = await getNextId("gateEntryCounter", "GE-");
-  const folder = `stock-management/gate-entry/${gateEntryId}`;
+  const folder = `stockmanagement/gateentry/${gateEntryId}`;
 
   // Upload files to Firebase Storage
   const [
@@ -200,7 +200,7 @@ exports.createStockEntry = asyncHandler(async (req, res) => {
   } = req.body;
 
   const stockEntryId = await getNextId("stockEntryCounter", "SE-");
-  const folder = `stock-management/stock-entry/${stockEntryId}`;
+  const folder = `stockmanagement/stockentry/${stockEntryId}`;
 
   const [uploadedRejectedPhoto, uploadedRejectedVideo] = await Promise.all([
     uploadBase64File(rejectedItemPhoto, folder, "rejectedItemPhoto"),
@@ -299,7 +299,7 @@ exports.createStockExit = asyncHandler(async (req, res) => {
   } = req.body;
 
   const stockExitId = await getNextId("stockExitCounter", "SX-");
-  const folder = `stock-management/gate-exit/${stockExitId}`;
+  const folder = `stockmanagement/stockexit/${stockExitId}`;
 
   const [uploadedExitPhoto, uploadedExitVideo] = await Promise.all([
     uploadBase64File(exitPhoto, folder, "exitPhoto"),
